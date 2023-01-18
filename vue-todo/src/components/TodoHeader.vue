1bb58a7765a9272e3e7c67d549e30f62b6ef4697
김상희 <kimsanghee@reflexion.co.kr>
2023-01-18 14:19:54 +0900

    todoInput 컴포넌트의 할일 저장 기능 구현

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <header>
+            <h1>TODO it!</h1>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+    h1{
+        color:#2f3d52;
+        font-weight:700;
+        margin:2.5rem 0 1.5rem;
+    }
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data : function(){
+        return {
+            newTodoItem : ''
+        }
+    },
+    methods : {
+        addTodo : function(){
+            // console.log(this.newTodoItem);
+            //저장하는 로직을 수행 후 인풋박스에 있는 내용을 비워주기 
+            
+            //localStorage.setItem(key,value);
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.newTodoItem ='';
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
