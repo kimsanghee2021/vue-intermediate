11b8b8f4ebf795263eeed4c4a5508f78f4dae939
김상희 <kimsanghee@reflexion.co.kr>
2023-01-31 15:39:20 +0900

    축약 함수 적용

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -59,10 +59,10 @@ export default {
     }
   },
   components : {
-    'TodoHeader': TodoHeader,
-    'TodoInput': TodoInput,
-    'TodoList': TodoList,
-    'TodoFooter': TodoFooter
+      TodoHeader,
+      TodoInput,
+      TodoList,
+      TodoFooter
   }
 }
 
